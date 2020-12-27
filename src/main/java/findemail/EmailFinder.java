2456f5852b5a394350cee1643a5f4fdfde0bdc8a
Spangevan <ewlson23@gmail.com>
2020-12-27 18:30:53 -0500

    added email list and main email finder application

---
+++ src/main/java/findemail/EmailFinder.java
@@ -0,0 +1,33 @@
+package findemail;
+
+public class EmailFinder {
+	
+	
+	public static void main (String[] args) {
+		
+		EmailList emailList = new EmailList(20);
+		
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Razzlefrazz@gmail.com");
+		
+		
+		
+	}
+
+}
---
+++ src/main/java/findemail/EmailList.java
@@ -0,0 +1,41 @@
+package findemail;
+
+public class EmailList {
+	String[] emails;
+	
+	int nextIndex = 0;
+	int arraySize = 0;
+	
+	public EmailList(int size) {
+		this.arraySize = size;
+		this.emails = new String[size];
+	}
+	
+	public void addEmail(String email) {
+		this.emails[this.nextIndex] = email;
+		this.nextIndex++;
+		if(this.nextIndex >= this.arraySize) {
+			this.nextIndex = 0;
+		}
+	}
+	
+	public void printEmails() {
+		for (int i = 0; i <this.emails.length; i++) {
+			
+			if (null == this.emails[i]) {
+				continue;
+			}
+			System.out.println(this.emails[i]);
+		}
+	}
+	
+	public boolean foundMatch(String input) {
+		for (int i = 0; i < this.emails.length; i++) {
+			if (this.emails[i].contentEquals(input)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}

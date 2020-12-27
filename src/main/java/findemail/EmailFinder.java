4a4faf597414e6bee028b3178874dd3a9d0448cc
Spangevan <ewlson23@gmail.com>
2020-12-27 18:34:14 -0500

    added emails to the String array

--- src/main/java/findemail/EmailFinder.java
+++ src/main/java/findemail/EmailFinder.java
@@ -8,23 +8,23 @@ public class EmailFinder {
 		EmailList emailList = new EmailList(20);
 		
 		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
-		emailList.addEmail("Razzlefrazz@gmail.com");
+		emailList.addEmail("Gatsby@yahoo.com");
+		emailList.addEmail("snowiscold@gmail.com");
+		emailList.addEmail("ilovejava@gmail.com");
+		emailList.addEmail("happychristmas@gmail.com");
+		emailList.addEmail("barb.sander@gmail.com");
+		emailList.addEmail("homer.simpson@gmail.com");
+		emailList.addEmail("peter.griffin@gmail.com");
+		emailList.addEmail("mac.miller@gmail.com");
+		emailList.addEmail("christina.giles@gmail.com");
+		emailList.addEmail("evan.spangenberg@gmail.com");
+		emailList.addEmail("monkeysee@gmail.com");
+		emailList.addEmail("monkeydoo@gmail.com");
+		emailList.addEmail("belowzero@gmail.com");
+		emailList.addEmail("sky.time@gmail.com");
+		emailList.addEmail("red.apples@gmail.com");
+		emailList.addEmail("blue.pear@gmail.com");
+		emailList.addEmail("happy.feet@gmail.com");
 		
 		
 		
